dda58bd3d64c235446e67b0049215e23aef20c8a
darren do <80792529+14flipz@users.noreply.github.com>
2026-04-27 19:50:05 -0400

    Update InvalidInputException.java

--- InvalidInputException.java
+++ InvalidInputException.java
@@ -1,9 +1,6 @@
 
 public class InvalidInputException extends Exception {
-
-    // Constructor that accepts a custom error message
     public InvalidInputException(String message) {
-        // Pass the message to the parent Exception class
         super(message);
     }
 }
